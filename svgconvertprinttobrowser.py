fb1645595fbdf23c6b9d28cb135695d186136d01
QinQing <northisland2017@gmail.com>
2023-10-11 21:39:42 +0800

    ADDED: svgconvertprinttobrowser.py最终版本以及原始文件printtobrowser.py

--- svgconvertprinttobrowser.py
+++ svgconvertprinttobrowser.py
@@ -2,7 +2,7 @@
 # Copyright 2008-2020 Jaap Karssenberg <jaap.karssenberg@gmail.com>
 
 '''Plugin to serve as work-around for the lack of printing support'''
-
+import urllib.parse
 import re
 from urllib.request import unquote
 from gi.repository import Gtk
@@ -25,19 +25,30 @@ from zim.gui.applications import open_url
 
 from zim.plugins.tasklist.gui import TaskListWindowExtension
 
+import logging
+logger = logging.getLogger('zim.plugins.myplugin')
+
+def deal_div(origin_str):
+    obj_iter = re.finditer("(<div style='[^<]+)('>)", origin_str)
+    right, final_str, last_part = 0, '', origin_str
+    for obj_sub in obj_iter:
+        if obj_sub.span()[0] > right:
+            final_str += origin_str[right:obj_sub.span()[0]]
+        right = obj_sub.span()[1]
+        part_1, part_2 = obj_sub.group(1), obj_sub.group(2)
+        semicolon_fit = ';' if part_1[-1] != ';' else ''
+        final_str += part_1 + semicolon_fit + 'margin: 0; font-family:"sarasa mono sc"; white-space: pre;' + part_2
+        last_part = origin_str[right:]
+    final_str += last_part
+    return final_str
+
 
 class PrintToBrowserPlugin(PluginClass):
 
 	plugin_info = {
-		'name': _('Print to Browser Convert Svg'), # T: plugin name
+		'name': _('my plugin'), # T: plugin name
 		'description': _('''\
-This plugin provides a workaround for the lack of
-printing support in zim. It exports the current page
-to html and opens a browser. Assuming the browser
-does have printing support this will get your
-data to the printer in two steps. 
-
-This is a core plugin shipping with zim.
+qinqing modify fo future use
 '''), # T: plugin description
 		'author': 'Jaap Karssenberg',
 		'help': 'Plugins:Print to Browser'
@@ -57,26 +68,23 @@ This is a core plugin shipping with zim.
 		lines = []
 		template.process(lines, context)
 		new_lines = []
+
 		for single_line in lines:
-			# <img src="file:///E:/zim_notebook/svg%E6%B5%8B%E8%AF%95/bob.svg">
-			svg_iter = re.finditer("<img src=\"([^<]+\.svg)\">", single_line)
-			right, final_str, svg_exist_flag, last_part = 0, '', False, ''
-			for sub_svg in svg_iter:
-				svg_exist_flag = True
-				if sub_svg.span()[0] > right:
-					final_str += single_line[right:sub_svg.span()[0]]
-				right = sub_svg.span()[1]
-				svg_path = unquote(sub_svg.group(1)[8:])
-				with open(svg_path, mode='r', encoding='utf8') as svg_text:
+			svg_iter = re.finditer("<p>\r?\n<img src=\"([^<]+\.svg)\">\r?\n</p>", single_line)
+			right, final_str, last_part = 0, '', single_line
+			for svg_sub in svg_iter:
+				if svg_sub.span()[0] > right:
+					final_str += deal_div(single_line[right:svg_sub.span()[0]])
+				right = svg_sub.span()[1]
+				real_path = urllib.parse.unquote(svg_sub.group(1)[8:])
+				with open(real_path, mode='r', encoding='utf8') as svg_text:
 					final_str += svg_text.read()
 				last_part = single_line[right:]
-			if svg_exist_flag and last_part:
-				final_str += last_part
-			if not svg_exist_flag:
-				final_str += single_line
+			final_str += deal_div(last_part)
 			new_lines.append(final_str)
-
+			
 		file.writelines(new_lines)
+
 		return file
 
 
