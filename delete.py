3ecf380433f522691aa190a0f188070df4c36bdc
qindapao <northisland2017@gmail.com>
2026-06-28 12:10:13 +0800

    CHANGED: zim plugins update.

--- delete.py
+++ delete.py
@@ -1,23 +1,110 @@
 # -*- coding: utf-8 -*-
+# This is a zim plugin to delete only the selected text and the attachments within that selection.
 
-# 这个文件的作用是删除范围内的文件
-
-import sys
+from zim.plugins import PluginClass
+from zim.gui.pageview import PageViewExtension
+from zim.actions import action
+from zim.gui.widgets import ErrorDialog, QuestionDialog
+import logging
 import pathlib
 import re
 
-TEXT, PAPER_NAME = sys.argv[1], sys.argv[2]
+logger = logging.getLogger('zim.plugins.delete_selection_scope')
 
+TOOLBAR_ICON = 'gtk-delete' 
+TOOLBAR_TOOLTIP = '删除选中范围内的文本和附件' 
 
-def deal_pics():
-    # zim中所有页面名字都去掉了下划线，要转换过来
-    real_path = pathlib.Path.cwd().joinpath(PAPER_NAME.split(':')[-1].replace(' ', '_'))
-    pic_iter = re.finditer(r"\{\{\.\\([^{]+\.(?:png|jpg|jpeg|svg))\}\}", TEXT, re.M)
-    for sub_pic in pic_iter:
-        pic_path = real_path.joinpath(sub_pic.group(1))
-        pic_path.unlink(missing_ok=True)
+class DeleteSelectionScopePlugin(PluginClass):
+    plugin_info = {
+        'name': 'Delete Selection Scope', 
+        'description': '通过行号比对，完美兼容 Zim 0.77.0 删除选区文本及其中包含的图片/附件', 
+        'author': '秦大炮', 
+        'help': 'Plugins:DeleteSelectionScope',
+    }
 
+class DeleteSelectionScopePageViewExtension(PageViewExtension):
 
-if __name__ == '__main__':
-    deal_pics()
+    def __init__(self, plugin, pageview):
+        PageViewExtension.__init__(self, plugin, pageview)
+        self.connectto(pageview, 'page-changed')
 
+    def on_page_changed(self, pageview, page, path):  
+        self._set_action_sensitivity()
+
+    def teardown(self):
+        self.disconnect_from(self.pageview)
+
+    def on_load_page(self, pageview, page):
+        self._set_action_sensitivity()
+
+    def _set_action_sensitivity(self):
+        page = self.pageview.page
+        self.actiongroup.set_sensitive(page is not None and not page.is_placeholder)
+
+    @action(TOOLBAR_TOOLTIP, icon=TOOLBAR_ICON) 
+    def delete_selected_scope(self):
+        page = self.pageview.page
+        if not page or not page.source_file:
+            return
+
+        # 1. 获取选中的文本区域行号
+        textview = self.pageview.textview
+        buffer = textview.get_buffer()
+        
+        has_selection = buffer.get_selection_bounds()
+        if not has_selection:
+            ErrorDialog(self.pageview, '未检测到选区，请先用鼠标选中一段文本！').run()
+            return
+            
+        start_iter, end_iter = has_selection
+        
+        # 精准拿到鼠标框选的起始行和结束行（从 0 开始算）
+        start_line = start_iter.get_line()
+        end_line = end_iter.get_line()
+
+        # 2. 弹出确认，防止误删
+        dialog = QuestionDialog(
+            self.pageview,
+            f"确定要删除选中行（第 {start_line+1} 行 到 第 {end_line+1} 行）内的文本及包含的附件吗？"
+        )
+        if not dialog.run():
+            return 
+
+        try:
+            # 3. 定位附件文件夹路径
+            txt_file_path = pathlib.Path(page.source_file.path)
+            current_dir = txt_file_path.parent
+            paper_name = page.name
+            real_path = current_dir.joinpath(paper_name.split(':')[-1].replace(' ', '_'))
+
+            # 4. 读取底层源码（因为 GTK 内存里吞图，物理源码永远说真话）
+            if txt_file_path.exists():
+                with open(txt_file_path, 'r', encoding='utf-8') as f:
+                    source_lines = f.readlines()
+
+                # 5. 精准切片：只在用户选中的这几行源码里抓取图片
+                # end_line + 1 是为了包含最后一行
+                selected_source_lines = source_lines[start_line:end_line + 1]
+                source_text_content = "".join(selected_source_lines)
+
+                # 6. 在选中的源码区间里匹配图片
+                pic_iter = re.finditer(r"\{\{\.[\\/]([^{]+\.(?:png|jpg|jpeg|svg))\}\}", source_text_content, re.M)
+                
+                if real_path.exists():
+                    deleted_count = 0
+                    for sub_pic in pic_iter:
+                        matched_filename = sub_pic.group(1).replace('\\', '/')
+                        clean_filename = matched_filename.split('/')[-1]
+                        pic_path = real_path.joinpath(clean_filename)
+                        
+                        if pic_path.exists():
+                            pic_path.unlink()
+                            deleted_count += 1
+                    logger.info(f"成功物理删除了选区内的 {deleted_count} 个附件。")
+
+            # 7. 无论源码对齐如何，界面选中的文本和图片必须立刻抹掉
+            buffer.delete(start_iter, end_iter)
+
+        except Exception as e:
+            logger.exception("删除选区附件时发生错误")
+            ErrorDialog(self.pageview, f'删除失败\n错误原因: {e}').run()
--- open_in_gvim.py
+++ open_in_gvim.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-# This is a zim plugin to open the current file in gvim and jump to the current line
-# You need to have emacsclient installed and running
-# You can change the emacsclient path and arguments below
-# You can also change the toolbar icon and the tooltip below
-
 from zim.plugins import PluginClass
 from zim.gui.pageview import PageViewExtension
 from zim.actions import action
@@ -12,23 +7,24 @@ import logging
 import subprocess
 import os
 
-# gvim是需要设置环境变量才能找到的
-os.environ['PATH'] += r";D:\programs\Vim\vim91"
+# 1. 安全地安全添加环境变量
+GVIM_PATH = r"D:\programs\Vim\vim91"
+if GVIM_PATH not in os.environ['PATH']:
+    os.environ['PATH'] = os.environ['PATH'].rstrip(os.pathsep) + os.pathsep + GVIM_PATH
+
 logger = logging.getLogger('zim.plugins.gvim')
 
-# The path and arguments of emacsclient
-GVIMLIENT = 'gvim'
+# 如果加入 PATH 还是报错，可以直接把这里改成 r"D:\programs\Vim\vim91\gvim.exe"
+GVIMLIENT = 'gvim' 
 
-# The toolbar icon and the tooltip of the plugin
-TOOLBAR_ICON = 'gtk-open' # You can use any stock icon or a custom icon file
-TOOLBAR_TOOLTIP = 'Open in Gvim' # T: tooltip
+TOOLBAR_ICON = 'zim-page' # 更换为 Zim 较通用的图标
+TOOLBAR_TOOLTIP = 'Open in Gvim' 
 
-class EmacsPlugin(PluginClass):
-
+class GvimPlugin(PluginClass): # 重命名类名，避免与 Emacs 混淆
     plugin_info = {
-        'name': 'Gvim', # T: plugin name
-        'description': 'This plugin adds a toolbar button to open the current file in gvim and jump to the current line', # T: plugin description
-        'author': 'Copilot', # T: plugin author
+        'name': 'Gvim', 
+        'description': 'This plugin adds a toolbar button to open the current file in gvim and jump to the current line', 
+        'author': 'Qindapao', 
         'help': 'Plugins:Gvim',
     }
 
@@ -38,7 +34,7 @@ class GvimPageViewExtension(PageViewExtension):
         PageViewExtension.__init__(self, plugin, pageview)
         self.connectto(pageview, 'page-changed')
 
-    def on_page_changed(self, pageview, page, path):  # 新增这个方法
+    def on_page_changed(self, pageview, page, path):  
         self._set_action_sensitivity()
 
     def teardown(self):
@@ -48,45 +44,27 @@ class GvimPageViewExtension(PageViewExtension):
         self._set_action_sensitivity()
 
     def _set_action_sensitivity(self):
-        # Only enable the action when the page is not a placeholder
         page = self.pageview.page
         self.actiongroup.set_sensitive(not page.is_placeholder)
 
-    @action(TOOLBAR_TOOLTIP, icon=TOOLBAR_ICON) # T: tooltip
+    @action(TOOLBAR_TOOLTIP, icon=TOOLBAR_ICON) 
     def open_in_gvim(self):
-        # Get the current page and its file path
         page = self.pageview.page
         file_path = page.source_file.path
 
-        # Get the current line number from the text buffer
         textview = self.pageview.textview
         buffer = textview.get_buffer()
         cursor = buffer.get_insert()
         buffer_iter = buffer.get_iter_at_mark(cursor)
         cur_line_number = buffer_iter.get_line()
-        line_number = cur_line_number + 5
+        line_number = cur_line_number + 5 # 保持你原本的偏移逻辑
 
-        # 防止代码中的'''导致得代码行号不准
-        # :TODO: 下面的代码并没有作用,因为这里迭代的文本也是不包含收缩的字符的
-        start_iter = buffer.get_start_iter()
-        while start_iter.get_line() < cur_line_number:
-            end_iter = start_iter.copy()
-            end_iter.forward_to_line_end()
-            line_text = start_iter.get_text(end_iter)
-            if line_text.rstrip() == "'''":
-                line_number += 1
-            start_iter.forward_line()
-
-
-        # Run gvimclient with the file path and the line number
+        # 尝试调用 gvim
         try:
-            # :TODO: 如果出现问题,可能需要指定emacs的相关环境变量的路径($env:HOME = "C:\Users\q00208337\AppData\Roaming")
-            #        但由于emacs是通过已有服务打开,所以不会有问题,gvim可能需要设置环境变量
-            # :TODO: 当前有个问题是图标没有显示出来
-            # :TODO: gvim也可以类似处理
-            subprocess.Popen([GVIMLIENT, '--remote', f'+{line_number}', file_path])
-        except:
-            # Show an error dialog if something goes wrong
-            ErrorDialog(self.pageview, 'Failed to run gvimclient').run()
-
+            # 加上 shell=True 允许 Windows 更好地检索环境变量
+            subprocess.Popen([GVIMLIENT, '--remote', f'+{line_number}', file_path], shell=True)
+        except Exception as e:
+            logger.exception("Failed to launch gvim")
+            # 这里的弹窗会带上具体的报错原因（例如：[Error 2] 系统找不到指定的文件）
+            ErrorDialog(self.pageview, f'Failed to run gvimclient\nError: {e}').run()
 
